ab9456ff0d7a11604f110d7a885852ea2ccbf2c2
bilykb <brettbilyk@gmail.com>
2021-08-18 19:35:56 +0000

    initial commit

---
+++ fetcher.js
@@ -0,0 +1,16 @@
+const source = process.argv[2];
+const destination = process.argv[3];
+// sets the necessary info from the command line to two variables
+const request = require('request');
+const fs = require('fs');
+
+
+const fetcher = function(source, destination, cb) {
+  
+};
+
+const writer = (body, destination) => {
+  
+};
+
+fetcher(source, destination, writer);
