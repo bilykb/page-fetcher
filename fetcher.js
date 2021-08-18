8dd515381acb5db19ee9bc1fd787b9e117805dea
bilykb <brettbilyk@gmail.com>
2021-08-18 19:36:46 +0000

    two working functions: request and writer

--- fetcher.js
+++ fetcher.js
@@ -6,11 +6,27 @@ const fs = require('fs');
 
 
 const fetcher = function(source, destination, cb) {
-  
+  request(source, (error, response, body) => {
+    if (response) {
+      console.log("We have a response");
+      cb(body, destination);
+    // a separate function which writes the content to a file
+    } else {
+      console.log("Check if the URL is valid");
+    }
+  });
 };
 
 const writer = (body, destination) => {
-  
+  fs.writeFile(destination, body, function(err) {
+    if (err) {
+      throw err;
+    } else {
+      const byteSize = fs.statSync(destination).size;
+      // finds the byte size of the created file
+      console.log(`Downloaded and saved: ${byteSize} bytes.`);
+    }
+  });
 };
 
 fetcher(source, destination, writer);
